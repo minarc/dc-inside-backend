7d4a0ec16e1b0c6935949587468ced4b688b0bde
Rumo <arfrumo@gmail.com>
2019-06-28 04:35:48 +0900

    firebase

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,23 @@
 var express = require('express')
 var router = express.Router()
-let path = require('path')
+const path = require('path')
 const redis = require('redis')
+const request = require('request')
+const fs = require('fs')
+var admin = require('firebase-admin')
+
+const serviceAccount = require('../scissor-c5cd4-firebase-adminsdk-xi9sk-c329f70d62.json')
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  storageBucket: 'gs://scissor-c5cd4.appspot.com'
+})
+
+let bucket = admin.storage().bucket()
+
+// 'bucket' is an object defined in the @google-cloud/storage library.
+// See https://googlecloudplatform.github.io/google-cloud-node/#/docs/storage/latest/storage/bucket
+// for more details.
 
 let redisSubscriber = redis.createClient({
   host: 'redis-10317.c16.us-east-1-3.ec2.cloud.redislabs.com',
@@ -14,9 +30,9 @@ let redisOperation = redis.createClient({
   port: 10317,
   password: 'WCkaZYzyhYR62p42VddCJba7Kn14vdvw'
 })
+
 redisSubscriber.subscribe('dc')
 
-/* GET home page. */
 router.get('/api/sse', (req, res, next) => {
   res.set({
     'Content-Type': 'text/event-stream',
@@ -30,21 +46,51 @@ router.get('/api/sse', (req, res, next) => {
   })
 })
 
-router.get('/api/depress', (req, res, next) => {
-  console.log(req.query.hash)
-
-  redisOperation.SADD('depress', req.query.hash, (error, response) => {
+router.post('/api/depress', (req, res, next) => {
+  redisOperation.SISMEMBER(req.body.hash, 'depress', (error, reply) => {
     if (error) {
       res.sendStatus(400)
     }
-    res.send()
+
+    if (reply == 0) {
+      redisOperation.SADD('depress', req.body.hash, (error, response) => {
+        if (error) {
+          res.sendStatus(400)
+        }
+        res.send(
+          JSON.stringify({
+            result: response
+          })
+        )
+      })
+      request({ uri: req.body.url, method: 'GET', encoding: null }, (error, response, body) => {
+        const content_disposition = response.headers['content-disposition']
+        const extension = content_disposition
+          .split(';')[1]
+          .split('=')[1]
+          .split('.')[1]
+
+        bucket.file(req.body.hash).save(body, { validation: false, resumable: false, contentType: 'image/jpg' })
+      })
+    } else {
+      res.send(
+        JSON.stringify({
+          result: 'already exist'
+        })
+      )
+    }
   })
 })
 
 router.get('/api/restrict', (req, res, next) => {
-  redisOperation.SMOVE('depress', 'restrict', req.query.hash, (error, response) => {
-    if (error) {
-      res.sendStatus(400)
+  redisOperation.SISMEMBER(req.query.hash, 'restrict', (error, reply) => {
+    if (reply == 0) {
+      redisOperation.SMOVE('depress', 'restrict', req.query.hash, (error, response) => {
+        if (error) {
+          res.sendStatus(400)
+        }
+        res.send()
+      })
     }
     res.send()
   })
