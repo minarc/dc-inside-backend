7e2bcd0a42c7e41e0f96542ff97dac680e21dfba
Rumo <arfrumo@gmail.com>
2019-08-31 03:47:02 +0900

    filtering

--- app.js
+++ app.js
@@ -1,42 +1,45 @@
-var createError = require('http-errors')
-var express = require('express')
-var path = require('path')
-var cookieParser = require('cookie-parser')
-var logger = require('morgan')
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
 
-var indexRouter = require('./routes/index')
+var indexRouter = require("./routes/index");
 
-var app = express()
+var app = express();
 
-console.log()
 // view engine setup
-app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'jade')
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "jade");
 
-app.use(logger('dev'))
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
-app.use(cookieParser())
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(
+  logger(
+    ":remote-addr - :remote-user [:date[clf]] ':method :url HTTP/:http-version' :status :response-time ms :res[content-length] ':referrer' ':user-agent'"
+  )
+);
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter)
+app.use("/", indexRouter);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
-  next(createError(404))
-})
+  next(createError(404));
+});
 
 // error handler
 app.use((err, req, res, next) => {
   // set locals, only providing error in development
-  res.locals.message = err.message
-  res.locals.error = req.app.get('env') === 'development' ? err : {}
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
-  res.status(err.status || 500)
-  res.render('error')
-})
+  res.status(err.status || 500);
+  res.render("error");
+});
 
-process.setMaxListeners(100)
+process.setMaxListeners(100);
 
-module.exports = app
+module.exports = app;
